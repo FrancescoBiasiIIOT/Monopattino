138fefa432c8c8c104db25634bc42ed2fea9ee54
MatteoMaccagnan <matteomac2000@gmail.com>
2020-12-16 17:27:13 +0100

    Models per monopattino e microcontrollore(Non definitivi)

---
+++ CLIENT/ITS.Monopattino.Client/ITS.Monopattino.Client.Models/Microcontrollore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITS.Monopattino.Client.Models
+{
+    public class Microcontrollore
+    {
+        public int Id;
+
+        public double Speed;
+
+        public int BatteryLvl;
+
+        public double Lat;
+
+        public double Lon;
+
+        public bool Power;
+
+        public int MaxKm;
+
+        public int RemainingKm;
+
+
+    }
+}
---
+++ CLIENT/ITS.Monopattino.Client/ITS.Monopattino.Client.Models/Monopattino.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITS.Monopattino.Client.Models
+{
+    public class Monopattino
+    {
+        public int Id { get; set; }
+
+        public int MicroId { get; set; }
+
+        public string Marca { get; set; }
+
+
+    }
+}
